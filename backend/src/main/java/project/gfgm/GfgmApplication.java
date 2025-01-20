fa6574398992b98eaac668c33193f96ddba84b9f
Thao <foreveralone2111997@gmail.com>
2025-01-20 15:40:01 +0700

    first commit

---
+++ backend/src/main/java/project/gfgm/GfgmApplication.java
@@ -0,0 +1,13 @@
+package project.gfgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfgmApplication.class, args);
+	}
+
+}
